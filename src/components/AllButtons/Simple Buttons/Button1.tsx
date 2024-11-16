465ed08995f0cf605f1329f8d09301649f3cfa6e
Surjendu kar <rahulgotp9988@gmail.com>
2024-11-16 21:08:33 +0530

    feat: build custom package for simplified MUI component usage

--- src/components/AllButtons/Simple Buttons/Button1.tsx
+++ src/components/AllButtons/Simple Buttons/Button1.tsx
@@ -1,182 +1,28 @@
-import { styled } from "@mui/material/styles";
-import { Button, Typography } from "@mui/material";
 import CodeViewer from "../../CodeViewer/CodeViewer";
-
-const LeftArrowButton = styled(Button)(() => ({
-  background: "red",
-  borderRadius: 4,
-  border: "none",
-  color: "#FFFFFF",
-  textAlign: "center",
-  fontSize: "15px",
-  padding: "7px",
-  width: "150px",
-  transition: "all 0.5s",
-  textTransform: "capitalize",
-  margin: "5px",
-  position: "relative",
-  "& p": {
-    display: "inline-block",
-    position: "relative",
-    transition: "0.5s",
-    paddingLeft: "0",
-  },
-  "& p:before": {
-    content: '"«"',
-    position: "absolute",
-    opacity: 0,
-    top: "-2px",
-    left: "-20px",
-    transition: "0.5s",
-  },
-  "&:hover p": {
-    paddingLeft: "25px",
-  },
-  "&:hover p:before": {
-    opacity: 1,
-    left: 0,
-  },
-}));
-
-const RightArrowButton = styled(Button)(() => ({
-  borderRadius: 4,
-  border: "none",
-  color: "#FFFFFF",
-  textAlign: "center",
-  fontSize: "15px",
-  padding: "7px",
-  width: "150px",
-  transition: "all 0.5s",
-  textTransform: "capitalize",
-  margin: "5px",
-  position: "relative",
-  "& p": {
-    display: "inline-block",
-    position: "relative",
-    transition: "0.5s",
-    paddingRight: "0",
-  },
-  "& p:after": {
-    content: '"»"',
-    position: "absolute",
-    opacity: 0,
-    top: "-2px",
-    right: "-20px",
-    transition: "0.5s",
-  },
-  "&:hover p": {
-    paddingRight: "25px",
-  },
-  "&:hover p:after": {
-    opacity: 1,
-    right: 0,
-  },
-}));
+import { LeftArrowButton } from "@byteui/mui-core";
+import { RightArrowButton } from "@byteui/mui-core";
+import { Box } from "@mui/material";
 
 const ButtonDemo = () => (
-  <>
-    <LeftArrowButton variant="contained">
-      <Typography>left</Typography>
-    </LeftArrowButton>
-    <RightArrowButton variant="contained">
-      <Typography>right</Typography>
-    </RightArrowButton>
-  </>
+  <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+    <LeftArrowButton>left</LeftArrowButton>
+    <RightArrowButton>Right</RightArrowButton>
+  </Box>
 );
 
 const Button1 = () => {
-  const shortCode = `<LeftArrowButton variant="contained">
-  <Typography>left</Typography>
-</LeftArrowButton>
+  const shortCode = `<LeftArrowButton>left</LeftArrowButton>
+<RightArrowButton>Right</RightArrowButton>`;
 
-<RightArrowButton variant="contained">
-  <Typography>right</Typography>
-</RightArrowButton>`;
-
-  const fullCode = `import React from "react";
-import { styled } from "@mui/material/styles";
-import { Button, Typography } from "@mui/material";
-
-const LeftArrowButton = styled(Button)(() => ({
-  background: "red",
-  borderRadius: 4,
-  border: "none",
-  color: "#FFFFFF",
-  textAlign: "center",
-  fontSize: "15px",
-  padding: "7px",
-  width: "150px",
-  transition: "all 0.5s",
-  textTransform: "capitalize",
-  margin: "5px",
-  position: "relative",
-  "& p": {
-    display: "inline-block",
-    position: "relative",
-    transition: "0.5s",
-    paddingLeft: "0",
-  },
-  "& p:before": {
-    content: '"«"',
-    position: "absolute",
-    opacity: 0,
-    top: "-2px",
-    left: "-20px",
-    transition: "0.5s",
-  },
-  "&:hover p": {
-    paddingLeft: "25px",
-  },
-  "&:hover p:before": {
-    opacity: 1,
-    left: 0,
-  },
-}));
-
-const RightArrowButton = styled(Button)(() => ({
-  borderRadius: 4,
-  border: "none",
-  color: "#FFFFFF",
-  textAlign: "center",
-  fontSize: "15px",
-  padding: "7px",
-  width: "150px",
-  transition: "all 0.5s",
-  textTransform: "capitalize",
-  margin: "5px",
-  position: "relative",
-  "& p": {
-    display: "inline-block",
-    position: "relative",
-    transition: "0.5s",
-    paddingRight: "0",
-  },
-  "& p:after": {
-    content: '"»"',
-    position: "absolute",
-    opacity: 0,
-    top: "-2px",
-    right: "-20px",
-    transition: "0.5s",
-  },
-  "&:hover p": {
-    paddingRight: "25px",
-  },
-  "&:hover p:after": {
-    opacity: 1,
-    right: 0,
-  },
-}));
+  const fullCode = `import { LeftArrowButton } from "@byteui/mui-core";
+import { RightArrowButton } from "@byteui/mui-core";
+import { Box } from "@mui/material";
 
 const ButtonDemo = () => (
-  <>
-    <LeftArrowButton variant="contained">
-      <Typography>left</Typography>
-    </LeftArrowButton>
-    <RightArrowButton variant="contained">
-      <Typography>right</Typography>
-    </RightArrowButton>
-  </>
+  <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+    <LeftArrowButton>left</LeftArrowButton>
+    <RightArrowButton>Right</RightArrowButton>
+  </Box>
 );
 
 export default ButtonDemo;`;
