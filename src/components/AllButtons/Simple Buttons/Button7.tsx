4b9a43d6939dc2e6d4a8008b09f7a90e562452f5
Surjendu kar <rahulgotp9988@gmail.com>
2024-11-20 14:50:30 +0530

    Add Custom3DButton in my package

--- src/components/AllButtons/Simple Buttons/Button7.tsx
+++ src/components/AllButtons/Simple Buttons/Button7.tsx
@@ -1,282 +1,57 @@
-import { FC } from "react";
-import { Box, Button, styled, keyframes, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import CodeViewer from "../../CodeViewer/CodeViewer";
+import { Custom3DButton } from "@byteui/mui-core";
 
-const glowAnimation = keyframes`
-  from {
-    top: -150%;
-  }
-  to {
-    top: 150%;
-  }
-`;
+const Button7 = () => {
+  const shortCode = `<Custom3DButton customColor="#f87171" customBgcolor="#450a0a">
+  Red Button
+</Custom3DButton>
 
-const StyledRedButton = styled(Button)(() => ({
-  backgroundColor: "#450a0a",
-  color: "#f87171",
-  border: "1px solid #f87171",
-  borderBottomWidth: "4px",
-  fontWeight: 500,
-  overflow: "hidden",
-  position: "relative",
-  padding: "6px 30px",
-  borderRadius: "6px",
-  outline: "none",
-  transition: "all 0.3s ease",
-  fontFamily: '"JetBrains Mono", monospace',
-  textTransform: "none",
+<Custom3DButton customColor="#4ade80" customBgcolor="#052e16">
+  Green Button
+</Custom3DButton>
 
-  "&:hover": {
-    filter: "brightness(1.5)",
-    borderTopWidth: "4px",
-    borderBottomWidth: "1px",
-  },
+<Custom3DButton customColor="#facc15" customBgcolor="#422006">
+  Yellow Button
+</Custom3DButton>`;
 
-  "&:active": {
-    opacity: 0.75,
-  },
+  const fullCode = `import { Box } from "@mui/material";
+import { Custom3DButton } from "@byteui/mui-core";
 
-  "& .glow-effect": {
-    backgroundColor: "#f87171",
-    boxShadow: "0 0 10px 10px rgba(248, 113, 113, 0.3)",
-    position: "absolute",
-    top: "-150%",
-    left: 0,
-    display: "inline-flex",
-    width: "320px",
-    height: "5px",
-    borderRadius: "6px",
-    opacity: 0.5,
-    transition: "all 0.5s ease",
-  },
-
-  "&:hover .glow-effect": {
-    animation: `${glowAnimation} 0.5s forwards`,
-  },
-}));
-
-const StyledGreenButton = styled(StyledRedButton)(() => ({
-  backgroundColor: "#052e16",
-  color: "#4ade80",
-  border: "1px solid #4ade80",
-
-  "& .glow-effect": {
-    backgroundColor: "#4ade80",
-    boxShadow: "0 0 10px 10px rgba(74, 222, 128, 0.3)",
-  },
-}));
-
-const StyledYellowButton = styled(StyledRedButton)(() => ({
-  backgroundColor: "#422006",
-  color: "#facc15",
-  border: "1px solid #facc15",
-
-  "& .glow-effect": {
-    backgroundColor: "#facc15",
-    boxShadow: "0 0 10px 10px rgba(250, 204, 21, 0.3)",
-  },
-}));
-
-const Button7: FC = () => {
-  const shortCode = `<StyledRedButton>
-  <Typography className="glow-effect"></Typography>
-  <Typography>Red Button</Typography>
-</StyledRedButton>
-
-<StyledGreenButton>
-  <Typography className="glow-effect"></Typography>
-  <Typography>Green Button</Typography>
-</StyledGreenButton>
-
-<StyledYellowButton>
-  <Typography className="glow-effect"></Typography>
-  <Typography>Yellow Button</Typography>
-</StyledYellowButton>`;
-
-  const fullCode = `import { FC } from "react";
-import { Box, Button, styled, keyframes, Typography } from "@mui/material";
-
-const glowAnimation = keyframes\`
-  from {
-    top: -150%;
-  }
-  to {
-    top: 150%;
-  }
-\`;
-
-const StyledRedButton = styled(Button)(() => ({
-  backgroundColor: "#450a0a",
-  color: "#f87171",
-  border: "1px solid #f87171",
-  borderBottomWidth: "4px",
-  fontWeight: 500,
-  overflow: "hidden",
-  position: "relative",
-  padding: "6px 30px",
-  borderRadius: "6px",
-  outline: "none",
-  transition: "all 0.3s ease",
-  fontFamily: '"JetBrains Mono", monospace',
-  textTransform: "none",
-
-  "&:hover": {
-    filter: "brightness(1.5)",
-    borderTopWidth: "4px",
-    borderBottomWidth: "1px",
-  },
-
-  "&:active": {
-    opacity: 0.75,
-  },
-
-  "& .glow-effect": {
-    backgroundColor: "#f87171",
-    boxShadow: "0 0 10px 10px rgba(248, 113, 113, 0.3)",
-    position: "absolute",
-    top: "-150%",
-    left: 0,
-    display: "inline-flex",
-    width: "320px",
-    height: "5px",
-    borderRadius: "6px",
-    opacity: 0.5,
-    transition: "all 0.5s ease",
-  },
-
-  "&:hover .glow-effect": {
-    animation: \`\${glowAnimation} 0.5s forwards\`,
-  },
-}));
-
-const StyledGreenButton = styled(Button)(() => ({
-  backgroundColor: "#052e16",
-  color: "#4ade80",
-  border: "1px solid #4ade80",
-  borderBottomWidth: "4px",
-  fontWeight: 500,
-  overflow: "hidden",
-  position: "relative",
-  padding: "6px 30px",
-  borderRadius: "6px",
-  outline: "none",
-  transition: "all 0.3s ease",
-  fontFamily: '"JetBrains Mono", monospace',
-  textTransform: "none",
-
-  "&:hover": {
-    filter: "brightness(1.5)",
-    borderTopWidth: "4px",
-    borderBottomWidth: "1px",
-  },
-
-  "&:active": {
-    opacity: 0.75,
-  },
-
-  "& .glow-effect": {
-    backgroundColor: "#4ade80",
-    boxShadow: "0 0 10px 10px rgba(74, 222, 128, 0.3)",
-    position: "absolute",
-    top: "-150%",
-    left: 0,
-    display: "inline-flex",
-    width: "320px",
-    height: "5px",
-    borderRadius: "6px",
-    opacity: 0.5,
-    transition: "all 0.5s ease",
-  },
-
-  "&:hover .glow-effect": {
-    animation: \`\${glowAnimation} 0.5s forwards\`,
-  },
-}));
-
-const StyledYellowButton = styled(Button)(() => ({
-  backgroundColor: "#422006",
-  color: "#facc15",
-  border: "1px solid #facc15",
-  borderBottomWidth: "4px",
-  fontWeight: 500,
-  overflow: "hidden",
-  position: "relative",
-  padding: "6px 30px",
-  borderRadius: "6px",
-  outline: "none",
-  transition: "all 0.3s ease",
-  fontFamily: '"JetBrains Mono", monospace',
-  textTransform: "none",
-
-  "&:hover": {
-    filter: "brightness(1.5)",
-    borderTopWidth: "4px",
-    borderBottomWidth: "1px",
-  },
-
-  "&:active": {
-    opacity: 0.75,
-  },
-
-    "& .glow-effect": {
-    backgroundColor: "#facc15",
-    boxShadow: "0 0 10px 10px rgba(250, 204, 21, 0.3)",
-    position: "absolute",
-    top: "-150%",
-    left: 0,
-    display: "inline-flex",
-    width: "320px",
-    height: "5px",
-    borderRadius: "6px",
-    opacity: 0.5,
-    transition: "all 0.5s ease",
-  },
-
-  "&:hover .glow-effect": {
-    animation: \`\${glowAnimation} 0.5s forwards\`,
-  },
-}));
-
-const ButtonDemo: FC = () => {
+const ButtonDemo = () => {
   return (
-    <Box sx={{ display: 'flex', gap: 2 }}>
-      <StyledRedButton>
-        <Typography className="glow-effect"></Typography>
-        <Typography>Red Button</Typography>
-      </StyledRedButton>
+    <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+      <Custom3DButton customColor="#f87171" customBgcolor="#450a0a">
+        Red Button
+      </Custom3DButton>
 
-      <StyledGreenButton>
-        <Typography className="glow-effect"></Typography>
-        <Typography>Green Button</Typography>
-      </StyledGreenButton>
+      <Custom3DButton customColor="#4ade80" customBgcolor="#052e16">
+        Green Button
+      </Custom3DButton>
 
-      <StyledYellowButton>
-        <Typography className="glow-effect"></Typography>
-        <Typography>Yellow Button</Typography>
-      </StyledYellowButton>
+      <Custom3DButton customColor="#facc15" customBgcolor="#422006">
+        Yellow Button
+      </Custom3DButton>
     </Box>
   );
 };
 
 export default ButtonDemo;`;
 
-  const ButtonDemo: FC = () => {
+  const ButtonDemo = () => {
     return (
       <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-        <StyledRedButton>
-          <Typography className="glow-effect"></Typography>
-          <Typography>Red Button</Typography>
-        </StyledRedButton>
+        <Custom3DButton customColor="#f87171" customBgcolor="#450a0a">
+          Red Button
+        </Custom3DButton>
 
-        <StyledGreenButton>
-          <Typography className="glow-effect"></Typography>
-          <Typography>Green Button</Typography>
-        </StyledGreenButton>
+        <Custom3DButton customColor="#4ade80" customBgcolor="#052e16">
+          Green Button
+        </Custom3DButton>
 
-        <StyledYellowButton>
-          <Typography className="glow-effect"></Typography>
-          <Typography>Yellow Button</Typography>
-        </StyledYellowButton>
+        <Custom3DButton customColor="#facc15" customBgcolor="#422006">
+          Yellow Button
+        </Custom3DButton>
       </Box>
     );
   };
