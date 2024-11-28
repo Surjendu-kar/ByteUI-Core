00f68f6535cab85325216b0ef4e1edc110aa73a9
Surjendu kar <rahulgotp9988@gmail.com>
2024-11-28 23:03:27 +0530

    fix loaders

--- src/components/AllLoaders/Loader6.tsx
+++ src/components/AllLoaders/Loader6.tsx
@@ -1,24 +1,27 @@
-import { ValoLoader } from "@byteui/mui-core";
+import { TextLoader, ValoLoader } from "@byteui/mui-core";
 import CodeViewer from "../CodeViewer/CodeViewer";
 import { CodeViewerContainer } from "../Shared/StyledComponents";
 
 const LoaderDemo = () => (
   <CodeViewerContainer isLoader>
     <ValoLoader size="50px" />
+    <TextLoader color="white" shadowColor="#000000" speed="1.5s" />
     <ValoLoader color="#0000ff" size="50px" speed="0.8s" />
   </CodeViewerContainer>
 );
 
 const Loader6 = () => {
-  const shortCode = `<ValoLoader size="50px" />
+  const shortCode = ` <ValoLoader size="50px" />
+<TextLoader color="white" shadowColor="#000000" speed="1.5s" />
 <ValoLoader color="#0000ff" size="50px" speed="0.8s" />`;
 
   const fullCode = `import { Box } from "@mui/material";
-import { ValoLoader } from "@byteui/mui-core";
+import { TextLoader, ValoLoader } from "@byteui/mui-core";
 
 const LoaderDemo = () => (
   <Box sx={{ display: "flex", gap: 10, alignItems: "center" }}>
     <ValoLoader size="50px" />
+    <TextLoader color="white" shadowColor="#000000" speed="1.5s" />
     <ValoLoader color="#0000ff" size="50px" speed="0.8s" />
   </Box>
 );
