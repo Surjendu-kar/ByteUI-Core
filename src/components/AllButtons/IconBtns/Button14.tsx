443bd7e077c2f413ed463ddeae3635622b54ae8b
Surjendu kar <rahulgotp9988@gmail.com>
2024-12-04 17:52:53 +0530

    Add DayLightToggle

--- src/components/AllButtons/IconBtns/Button14.tsx
+++ src/components/AllButtons/IconBtns/Button14.tsx
@@ -1,25 +1,36 @@
 import CodeViewer from "../../CodeViewer/CodeViewer";
-import { DayLightIcon } from "@byteui/mui-core";
+import { DayLightIcon, DayLightToggle } from "@byteui/mui-core";
 import { CodeViewerContainer } from "../../Shared/StyledComponents";
+import { useState } from "react";
 
 const ButtonDemo = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
     <CodeViewerContainer>
       <DayLightIcon />
+
+      <DayLightToggle active={isDark} onToggleChange={setIsDark} />
     </CodeViewerContainer>
   );
 };
 
 const Button14 = () => {
-  const shortCode = `<DayLightIcon />`;
+  const shortCode = `<DayLightIcon />
+<DayLightToggle active={isDark} onToggleChange={setIsDark} />`;
 
   const fullCode = `import { Box } from "@mui/material";
-import { DayLightIcon } from "@byteui/mui-core";
+import { DayLightIcon, DayLightToggle } from "@byteui/mui-core";
+import { useState } from "react";
+
 
 const ButtonDemo = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
     <Box sx={{ display: "flex", alignItems: "center",flexWrap: "wrap",justifyContent: "center", gap: 2 }}>
       <DayLightIcon />
+      <DayLightToggle active={isDark} onToggleChange={setIsDark} />
     </Box>
   );
 };
